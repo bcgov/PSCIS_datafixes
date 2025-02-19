077bed5b20b1a14798ca21aad47cc314325fcc43
Simon Norris <snorris@hillcrestgeo.ca>
2025-02-19 15:52:52 -0800

    fix #22

--- 2025-03-15/pscis_datafix_20250315.sql
+++ 2025-03-15/pscis_datafix_20250315.sql
@@ -9,6 +9,13 @@ UPDATE pscis.pscis_attachments
 SET assessment_id = 200810
 WHERE attachment_id IN (1080939,1080940,1080941,1080942,1080943,1080944,1080945,1080946);
 
-
+-- ----------------------------------
+-- https://github.com/smnorris/PSCIS_datafixes/issues/22
+-- ----------------------------------
+UPDATE pscis.pscis_stream_cross_loc_point
+SET utm_zone = 10, utm_easting = 515158, utm_northing = 6093463,
+geometry = SDO_GEOMETRY(2001,3005,SDO_POINT_TYPE(1206572.6165631632 1114594.8888236226, NULL),NULL,NULL),
+who_updated = 'DATAFIX_20250217', when_updated = sysdate
+WHERE stream_crossing_id = 197488;
 
 
